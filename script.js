d04e4a622696e507ee7e6062f9ce9227d319b4d9
Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>
2023-10-22 22:12:25 +0700

    finishing
    Signed-off-by: Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>

--- script.js
+++ script.js
@@ -21,3 +21,88 @@ var typed = new Typed('.typed', {
   backDelay: 3000,
   loop: true,
 });
+
+// JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+})();
+
+// function validate_form() {
+//   var first_name = document.getElementById('first-name');
+//   var last_name = document.getElementById('last-name');
+//   var phone_number = document.getElementById('phone-number');
+//   var email = document.getElementById('email');
+//   var selectMenuType = document.getElementById('selectMenuType');
+//   var description_text = document.getElementById('description-text');
+//   var invalidCheck = document.getElementById('invalidCheck');
+
+//   var firstNameInvalid = document.getElementById('first-name-invalid-feedback');
+//   var lastNamInvalid = document.getElementById('last-name-invalid-feedback');
+//   var phoneInvalid = document.getElementById('phone-invalid-feedback');
+//   var emailInvalid = document.getElementById('email-invalid-feedback');
+//   var typeInvalid = document.getElementById('type-invalid-feedback');
+//   var descriptionInvalid = document.getElementById('description-invalid-feedback');
+//   var checkInvalid = document.getElementById('check-invalid-feedback');
+
+//   // cek yang salah
+//   if (first_name.value == '') {
+//     first_name.focus();
+//     firstNameInvalid.style.display = 'block';
+//     return false;
+//   }
+
+//   if (last_name.value == '') {
+//     last_name.focus();
+//     nameInvalid.style.display = 'block';
+//     return false;
+//   }
+
+//   if (phone_number.value == '') {
+//     phone_number.focus();
+//     phoneInvalid.style.display = 'block';
+//     return false;
+//   }
+
+//   if (email.value == '') {
+//     email.focus();
+//     emailInvalid.style.display = 'block';
+//     return false;
+//   }
+
+//   if (selectMenuType.value == '') {
+//     selectMenuType.focus();
+//     emailInvalid.style.display = 'block';
+//     return false;
+//   }
+
+//   if (description_text.value == '') {
+//     description_text.focus();
+//     emailInvalid.style.display = 'block';
+//     return false;
+//   }
+
+//   if (invalidCheck.value == '') {
+//     invalidCheck.focus();
+//     messageInvalid.style.display = 'block';
+//     return false;
+//   }
+// }
